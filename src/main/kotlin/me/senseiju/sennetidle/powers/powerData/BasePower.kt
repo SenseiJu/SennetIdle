50178052af4d328b54580a3522e8f55477a3afcf
Fahad202136805 <91560862+FahadKhan2024@users.noreply.github.com>
2022-10-29 21:12:21 +0100

    Functions made on basepower and fireball/adrenaline. Added message.

--- src/main/kotlin/me/senseiju/sennetidle/powers/powerData/BasePower.kt
+++ src/main/kotlin/me/senseiju/sennetidle/powers/powerData/BasePower.kt
@@ -1,5 +1,6 @@
 package me.senseiju.sennetidle.powers.powerData
 
+import me.senseiju.sennetidle.powers.Power
 import me.senseiju.sennetidle.user.User
 import me.senseiju.sennetidle.utils.ModelItem
 
@@ -9,5 +10,17 @@ interface BasePower : ModelItem {
     val cooldown: Int
     val slot: Int
 
-    fun onRightClick(user: User)
+    fun onActivate(user: User)
+
+    fun setCooldown(user: User, power: Power) {
+        user.powerCooldowns[power] = System.currentTimeMillis()
+    }
+
+    fun isReady(user: User, power: Power): Boolean {
+        return System.currentTimeMillis() - user.powerCooldowns.getOrDefault(power, 0) <= cooldown * 1000
+    }
+
+    fun timeRemaining(user: User, power: Power): Long {
+        return cooldown * 1000 - (System.currentTimeMillis() - user.powerCooldowns.getOrDefault(power, 0))
+    }
 }
